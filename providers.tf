d9e8450c161890e00960cfdb2f33066403458d49
skolay-infostrux <103535202+skolay-infostrux@users.noreply.github.com>
2023-08-14 23:01:49 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,16 +8,6 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "238568438533-ca-central-1-snowflake-terraform-state"
-    dynamodb_table = "238568438533-ca-central-1-snowflake-terraform-state-locks"
-    encrypt        = "true"
-    key            = "data/snowflake/terraform.tfstate"
-    kms_key_id     = "alias/data/s3/238568438533-ca-central-1-snowflake-terraform-state"
-    region         = "ca-central-1"
-  }
-}
-
 provider "snowflake" {
   alias = "useradmin"
   role  = "USERADMIN"
